75ca4608b946a166505a129c73fe719ed01a5587
Patrick Gordon <patrickgordon42@yahoo.com>
2019-07-29 12:59:37 -0500

    started

---
+++ src/Components/Buttons.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function buttonCreator(props) {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+export default Buttons
---
+++ src/Components/Topnav.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Topnav() {
+    return (
+        <div className="topNavDiv">
+            <div className="logoDiv">
+                <img className="logo">LOGO HERE</img>
+            </div>
+            <div className="navProfileDiv">
+                <img className="profilePicture">USER PROFILE PICTURE HERE</img>
+            </div>
+        </div>
+    )
+}
