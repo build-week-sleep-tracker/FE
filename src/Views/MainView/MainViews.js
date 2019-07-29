09dce30ff7222454b0ffb3ce229b8b12299deec2
Patrick Gordon <patrickgordon42@yahoo.com>
2019-07-29 15:57:20 -0500

    main-view

---
+++ src/Views/MainView/MainViews.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Holds all the components for the Home/Main page
+
+export default function MainViews() {
+    return (
+        <div>
+            <TopNav />
+            <DashBoards />
+        </div>
+    )
+}
