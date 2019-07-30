74c18ba0d6ce4f0418ea5b5eceb45de9d3e238fc
Connor Claxton <czclaxton@gmail.com>
2019-07-29 20:23:47 -0500

    finished navbar component

--- src/Components/Topnav.js
+++ src/Components/Topnav.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import logo from "../images/logo.svg";
+import ethan from "../images/ethan.jpg";
 
-export default function Topnav() {
-    return (
-        <div className="topNavDiv">
-            <div className="logoDiv">
-                <img className="logo">LOGO HERE</img>
-            </div>
-            <div className="navProfileDiv">
-                <img className="profilePicture">USER PROFILE PICTURE HERE</img>
-            </div>
-        </div>
-    )
+const NavWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100vw;
+  background-color: #100359;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Logo = styled.img`
+  padding: 3px;
+  width: 75px;
+`;
+
+const ProfilePic = styled.img`
+  border-radius: 100%;
+  width: 50px;
+  height: 50px;
+  padding: 3px;
+`;
+
+export default function Topnav(props) {
+  return (
+    <NavWrapper>
+      <Logo src={logo} />
+      <ProfilePic src={ethan} />
+    </NavWrapper>
+  );
 }
