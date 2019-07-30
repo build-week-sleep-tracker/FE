ec15319dd1dff5fd9d3cdb69b0db2a0bdbeed331
Patrick Gordon <patrickgordon42@yahoo.com>
2019-07-30 14:28:16 -0500

    home page titles done, neeed styling

--- src/Components/Buttons/Titles.js
+++ src/Components/Buttons/Titles.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
+import Bed from '../../images/Bed.svg';
+import Clock from '../../images/Clock.svg';
+import Device from '../../images/Device.svg';
+import Face from '../../images/Face.svg';
+import Star from '../../images/Star.svg';
 
 
 const Container = styled.div`
@@ -19,6 +24,8 @@ text-align: center;
 background-color:#BFB6F1
 font-size: 30px;
 padding-top: 20px;
+display: flex;
+flex-direction: row;
 `
 const Alarm = styled.div`
 height: 300px;
@@ -28,6 +35,8 @@ text-align: center;
 background-color:#9D90EA;
 font-size: 30px;
 padding-top: 20px;
+display: flex;
+flex-direction: row;
 `
 const SleepQ = styled.div`
 height: 300px;
@@ -38,6 +47,8 @@ background-color: #6F5BE0
 color: white;
 font-size: 30px;
 padding-top: 20px;
+display: flex;
+flex-direction: row;
 
 `
 const Moods = styled.div`
@@ -49,6 +60,8 @@ background-color: #4D35D4
 color: white;
 font-size: 30px;
 padding-top: 20px;
+display: flex;
+flex-direction: row;
 
 `
 const Devices = styled.div`
@@ -60,6 +73,8 @@ background-color:#3421A1
 color: white;
 font-size: 30px;
 padding-top: 20px;
+display: flex;
+flex-direction: row;
 `
 const Insights = styled.div`
 height: 300px;
@@ -70,27 +85,35 @@ background-color:#100359
 color: white;
 font-size: 30px;
 padding-top: 20px;
+display: flex;
+flex-direction: row;
 `
 
 export default function Titles() {
     return (
         <Container>
         <LogSleep>
+            <img src={Bed} />
             <h3>Log Sleep</h3>
         </LogSleep>
         <Alarm>
+            <img src={Clock} />
             <h3>Alarm</h3>
         </Alarm>
         <SleepQ>
+        <img src={Star} />
             <h3>Sleep Quality</h3>
         </SleepQ>
         <Moods>
+        <img src={Face} />
             <h3>Moods</h3>
         </Moods>
         <Devices>
+        <img src={Device} />
             <h3>Devices</h3>
         </Devices>
         <Insights>
+        <img src={Star} />
             <h3>Insights</h3>
         </Insights>
         </Container>
