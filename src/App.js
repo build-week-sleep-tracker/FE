ef91550ce9f9f116385dd63b3ada9ce8de50ea74
AaronJThompson <aaronjthompson98@gmail.com>
2019-07-31 21:15:12 +0100

    Prepare for merge

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Provider } from "react-redux";
-// import { createStore, applyMiddleware } from "redux";
-// import combinedReducers from './Reducers';
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import combinedReducers from './Reducers';
 import GraphCanvas from './Components/Graph'
 
-// const store = createStore(
-//   combinedReducers,
-//   {},
-//   applyMiddleware(thunk)
-// );
+const store = createStore(
+  combinedReducers,
+  {},
+  applyMiddleware(thunk)
+);
 
 
 function App() {
   return (
-//    <Provider store={store}>
+    <Provider store={store}>
       <div className="App">
-      <GraphCanvas/>
       </div>
-//    </Provider>
+    </Provider>
   );
 }
 
