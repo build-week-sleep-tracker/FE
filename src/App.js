84a9a22d93811f54f38642cc17e84f887c73005a
Connor Claxton <czclaxton@gmail.com>
2019-07-31 10:26:08 -0500

    update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import combinedReducers from "./Reducers";
 import ReduxThunk from "redux-thunk";
-import Titles from './Components/Buttons/Titles';
 
 const store = createStore(combinedReducers, {}, applyMiddleware(ReduxThunk));
 
@@ -11,7 +10,6 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App" />
-      <Titles />
     </Provider>
   );
 }
