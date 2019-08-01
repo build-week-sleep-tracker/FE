3992dcb3b9e50fccca78606d40fa94f3611cc9df
AaronJThompson <aaronjthompson98@gmail.com>
2019-08-01 03:39:16 +0100

    Add routes for app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter as Router, Redirect, Route } from 'react-router-dom';
 import thunk from 'redux-thunk';
 import combinedReducers from './Reducers';
 import GraphCanvas from './Components/Graph'
 import MainViews from "./Views/MainView/MainViews";
-import Insights from './Components/Insights/Insights'
+import LoginView from './Views/LoginView';
+import RegisterView from './Views/RegisterView';
+import SleepView from './Views/SleepView';
+import TrackerInput from './Components/Trackers/TrackerLogInput';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import theme from './Styling/theme';
+import Cookies from 'universal-cookie';
 
 const store = createStore(
   combinedReducers,
@@ -13,10 +20,34 @@ const store = createStore(
   applyMiddleware(thunk)
 );
 
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  const cookies = new Cookies();
+  const session = cookies.get('sessionID');
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        session ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+}
+
 function App() {
   return (
     <Provider store={store}>
-     <Insights />
+      <Router>
+        <MuiThemeProvider theme={theme}>
+          <PrivateRoute exact path='/' component={MainViews} />
+          <Route path='/login' component={LoginView} />
+          <Route path='/register' component={RegisterView} />
+          <PrivateRoute path='/sleeps' component={SleepView} />
+        </MuiThemeProvider>
+      </Router>
     </Provider>
   );
 }
